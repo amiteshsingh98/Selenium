f96cde3e58e025843984b4cc9593d8dbc15c3ed9
amite <amite@Amitesh>
2024-03-13 11:53:52 +0530

    Added Few Lines of Code

---
+++ Selenium_Test/src/main/java/DifferentElements/RadioButtonTest.java
@@ -0,0 +1,39 @@
+package DifferentElements;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class RadioButtonTest {
+
+	public static void main(String[] args) {
+
+		WebDriver driver = new ChromeDriver();
+
+		driver.manage().window().maximize();
+
+		driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+
+		WebElement Radiobutton1 = driver.findElement(By.id("my-radio-1"));
+
+		Boolean isSelected = Radiobutton1.isSelected();
+
+		System.out.println(isSelected);
+
+		WebElement Radiobutton2 = driver.findElement(By.id("my-radio-2"));
+
+		//Boolean isSelected2 = Radiobutton2.isSelected();
+
+		//System.out.println(isSelected2);
+		
+		Boolean isDisplay = Radiobutton2.isDisplayed();
+		
+		System.out.println(isDisplay);
+		
+		
+
+
+	}
+
+}
